c99b08f3b8a8abcf52c15578f9d6872ca66666d0
Gabriel Lima Dantas <101371170+PeDeLaranjaLima@users.noreply.github.com>
2025-06-24 09:54:03 -0300

    Projeto de calculadora
    Calculadora
    Objetivo: usando uma aplicação do tipo console do dotnet, criar uma calculadora simples que terá as operações de soma, subtração, multiplicação e divisão. Faça o controle de versionamento da sua aplicação com git e suba a mesma para o github.
    
    Novos conceitos ou funcionalidades desse exercício
    Nossa primeira aplicação é bem simples e, se você já tem conhecimentos em lógica de programação, algoritmos e alguma linguagem de programação não terá problemas para desenvolver a aplicação. Faremos uso de operadores condicionais, estrutura condicional 'switch', criação e chamada de métodos simples.
    
    Requisitos para desenvolver a aplicação
    Ao entrar na calculadora o usuário deverá ter um menu com as opções 1, 2, 3, 4 e 0. Sendo elas:
    
    1 - Somar
    
    2 - Subtrair
    
    3 - Multiplicar
    
    4 - Dividir
    
    0 - Sair
    
    Todas as operações devem solicitar que o usuário entre com dois valores e então estes valores devem ser utilizados para realizar a operação escolhida. Exemplo de fluxo da operação de Soma:
    
    Aplicação solicita o primeiro valor;
    Usuário digita valor e pressiona enter;
    Aplicação solicita o segundo valor;
    Usuário digita valor e pressiona enter;
    Aplicação escreve em tela o resultado da soma;
    Usuário pressiona Enter;
    Aplicação retorna para o menu;
    Para a operação de divisão, validar se o divisor é 0. Se verdadeiro deve ser exibida a mensagem "Não é possível dividir por zero."
    
    Opcional
    
    Fazer o controle de versionamento da sua aplicação usando git;
    Subir a aplicação para um repositório do github.
    Desafios
    Crie uma nova opção na calculadora (opção número 5 do menu) que retorne o resto da divisão.
    Crie uma nova opção na calculadora (opção número 6 do menu) que retorne o resultado da potenciação de uma determinada base pelo seu expoente. Exemplo 2³ = 8.

---
+++ Calculadora.cs
@@ -0,0 +1,85 @@
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        // Console.Read() -> lê um caractere apenas
+        // Console.ReadLine() -> lê a linha até receber um enter
+
+        int op;
+
+        do
+        {
+            Console.WriteLine("Digite qual operação deseja realizar:");
+            Console.WriteLine("1 - Somar\n2 - Subtrair\n3 - Multiplicar\n4 - Dividir\n0 - Sair");
+
+            op = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("\nDigite o primeiro valor: ");
+                    int sum1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite o segundo valor: ");
+                    int sum2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine($"\n{sum1} + {sum2} = {sum1 + sum2}\n");
+
+                    break;
+                case 2:
+
+                    Console.WriteLine("\nDigite o primeiro valor: ");
+                    int sub1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite o segundo valor: ");
+                    int sub2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine($"\n{sub1} - {sub2} = {sub1 - sub2}\n");
+
+                    break;
+                case 3:
+
+                    Console.WriteLine("\nDigite o primeiro valor: ");
+                    int mut1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite o segundo valor: ");
+                    int mut2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine($"\n{mut1} x {mut2} = {mut1 * mut2}\n");
+
+                    break;
+                case 4:
+
+                    // Os dois valores tem que ser double, quando testado com int, o resultado era zero
+                    Console.WriteLine("\nDigite o primeiro valor: ");
+                    double div1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite o segundo valor: ");
+                    double div2 = Convert.ToDouble(Console.ReadLine());
+
+                    if (div2 == 0.0)
+                    {
+                        Console.WriteLine("Erro ao gerar divisão por zero, tente novamente");
+                        op = 4;
+                    }
+                    else
+                    {
+                        // Nem mesmo por (float) antes da operação deu certo
+                        Console.WriteLine($"\n{div1} / {div2} = {div1 / div2}\n");
+                    }
+
+                    break;
+                case 0:
+                    Console.WriteLine("\nPrograma finalizado...");
+                    op = 0;
+                    break;
+            }
+        } while (op != 0);
+
+
+    }
+}
